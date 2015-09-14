ede1712a6994751603b40017801abca9eb5a31f3
Nickolas Nikolic <nickolasnikolic@gmail.com>
2015-09-14 11:48:14 -0500

    removed libraries

--- js/main.js
+++ js/main.js
@@ -1,34 +1,29 @@
 $(document).ready(function(){
-  enquire.register("screen and (min-width:921px)",{
-    match: function(){
-      $('article').not('.portfolio').attr('data-sr', 'enter left, scale down 20% over 1.2s');
-      var s = skrollr.init();
-      (function getSpidey(){
-        console.log('running spidey');
-        var holder = [];
-        $.getJSON('api/spidey')
-            .success(function(result){
-              _.mapObject(result, function(value){
+  var s = skrollr.init();
+  (function getSpidey(){
+    console.log('running spidey');
+    var holder = [];
+    $.getJSON('api/spidey')
+        .success(function(result){
+          _.mapObject(result, function(value){
 
-                holder.push(value);
+            holder.push(value);
 
-              });
+          });
 
-              $( '#spinner i' ).removeClass('fa-5x');
+          $( '#spinner i' ).removeClass('fa-5x');
 
-              var count = 0;
-              (function runQue(){
-                console.log('running runQue');
-                $('#spinnerText').text('crawled: ' + holder[count++].title + ' awaiting...');
-                if(count == holder.length - 1){
-                  count = 0
-                }
-                setTimeout(runQue, 150);
-              })()
-            });
-      })()
-    }
-  });
+          var count = 0;
+          (function runQue(){
+            console.log('running runQue');
+            $('#spinnerText').text('crawled: ' + holder[count++].title + ' awaiting...');
+            if(count == holder.length - 1){
+              count = 0
+            }
+            setTimeout(runQue, 150);
+          })()
+        });
+  })()
 
   $.scrollIt({
     upKey: 38,             // key code to navigate to the next section
