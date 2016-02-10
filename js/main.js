a9fde4fe0d0a6bf10b21ee69a49314b3ec929aff
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-02-09 21:45:07 -0600

    color

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     return color;
   }
 
-  for(var i = 10; i > 0; i--){
+  for(var i = 50; i > 0; i--){
     var randomValue = Math.random();
     var circle = s.circle(  randomValue * width, randomValue * height, randomValue * height );
 
