6a709772f1badc3bd83286258bfbc696b29eedc1
Nickolas Nikolic <nickolasnikolic@gmail.com>
2016-02-09 23:07:30 -0600

    color

--- js/main.js
+++ js/main.js
@@ -75,7 +75,7 @@ $(document).ready(function(){
       $(this).velocity({
         cx: Math.random() * width,
         cy: Math.random() * height,
-        opacity: Math.random() * 0.65
+        opacity: Math.random() * 0.95
       }, speed, function () {
         animateDiv();
       });
